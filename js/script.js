21de924778d96317128e5722f69a0c71dcae7467
Dan.Arellano <daniel.arellano@cua.uam.mx>
2026-08-23 19:20:40 -0600

    se agrega contenido al archivo script.js

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// Guarda el nombre de la página.
+let nombrePagina = "Bandas por Década";
+
+// Guarda la década destacada del momento.
+let decadaDestacada = "2020s";
+
+// Guarda el número total de décadas mostradas.
+let totalDecadas = 7;
+
+// Guarda el número de recomendaciones por década.
+let recomendacionesPorDecada = 3;
+
+// Indica si la página tiene enlaces a redes sociales disponibles.
+let tieneRedesSociales = true;
+
+// Mostramos los valores en la consola.
+console.log(nombrePagina);
+console.log(decadaDestacada);
+console.log(totalDecadas);
+console.log(recomendacionesPorDecada);
+console.log(tieneRedesSociales);
